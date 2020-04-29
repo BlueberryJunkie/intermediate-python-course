ff4690bc5a91ba0eb7459b4eb70472d0fce69883
BlueberryJunkie <stan.rybachkin@gmail.com>
2020-04-29 13:09:17 +0300

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,8 @@
 import random
 def main():
     dice_rolls = int(input('How many dice would you like to roll?'))
-    dice_sum = 0
     dice_size = int(input('How many sides are the dice?'))
+    dice_sum = 0
     for i in range(0, dice_rolls):
         roll = random.randint(1, dice_size)
         dice_sum = dice_sum + roll
